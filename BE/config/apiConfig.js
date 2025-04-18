a667ea36f17beb1da9fca0e52be404d6f3093461
bananahun <vision981111@gmail.com>
2025-04-18 17:29:30 +0900

    feat-6-riotApi일부

--- BE/config/apiConfig.js
+++ BE/config/apiConfig.js
@@ -2,4 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const riotApiKey = process.env.RIOT_API_KEY;
-export const baseUrl = "https://kr.api.riotgames.com"; // 필요시 다른 지역 설정도 가능
+export const baseUrlKr = "https://kr.api.riotgames.com";
+export const baseUrlAsia = "https://asia.api.riotgames.com";
--- BE/routes/tft.js
+++ BE/routes/tft.js
@@ -1,29 +1,50 @@
-// routes/tft.js
-import express from "express";
+import express, { application, json } from "express";
 import axios from "axios";
-import { riotApiKey, baseUrl } from "../config/apiConfig.js"; // config에서 불러오기
+import { riotApiKey, baseUrlAsia, baseUrlKr } from "../config/apiConfig.js"; // config에서 불러오기
 
 const router = express.Router();
 
-// 챌린저 리그 정보 가져오기
-router.get("/challenger", async (req, res) => {
+// Riot ID로 계정 정보 받기
+router.get("/account/:gameName/:tagLine", async (req, res) => {
   try {
-    // axios를 이용해서 Riot API에 GET 요청을 보냄
-    const response = await axios.get(
-      `${baseUrl}/tft/league/v1/challenger`, // baseUrl을 사용해서 완전한 URL을 생성
-      {
-        headers: {
-          "X-Riot-Token": riotApiKey, // 헤더에 Riot API 키를 넣어서 요청
-        },
-      }
-    );
-    res.json(response.data); // 요청 성공 시 응답 데이터를 JSON 형태로 반환
+    const { gameName, tagLine } = req.params;
+    const encodedGameName = encodeURIComponent(gameName);
+    const encodedTagLine = encodeURIComponent(tagLine);
+
+    const riot_url = `${baseUrlAsia}/riot/account/v1/accounts/by-riot-id/${encodedGameName}/${encodedTagLine}`;
+    console.log(riot_url);
+    const response = await axios.get(riot_url, {
+      headers: {
+        "X-Riot-Token": riotApiKey,
+        "Content-Type": application / json,
+      },
+    });
+    console.log(response.headers);
+    res.json(response.data);
   } catch (error) {
     console.error(
-      "Error fetching challenger league:",
+      "에러 - 라이엇 아이디로 계정 정보 받기:",
       error.response?.data || error.message
     );
-    res.status(500).json({ message: "Failed to fetch challenger info" }); // 실패 시 에러 메시지 반환
+    res.status(500).json({ message: "실패 - 라이엇 아이디로 계정 정보 받기" });
+  }
+});
+
+// 챌린저 리그 정보 받기
+router.get("/challenger", async (req, res) => {
+  try {
+    const response = await axios.get(`${baseUrlKr}/tft/league/v1/challenger`, {
+      headers: {
+        "X-Riot-Token": riotApiKey,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(
+      "에러 - 챌린저 리그 정보 받기:",
+      error.response?.data || error.message
+    );
+    res.status(500).json({ message: "실패 - 챌린저 리그 정보 받기" });
   }
 });
 
